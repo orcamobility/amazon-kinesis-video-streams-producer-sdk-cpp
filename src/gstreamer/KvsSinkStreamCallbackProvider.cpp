341bf8954f17d971eea014da58b77187890ea211
Balazs Kovacs <balazs@orcamobility.ai>
2024-05-01 16:02:16 -0700

    Increase log level for logs in stream callbacks

--- src/gstreamer/KvsSinkStreamCallbackProvider.cpp
+++ src/gstreamer/KvsSinkStreamCallbackProvider.cpp
@@ -7,9 +7,10 @@ using namespace com::amazonaws::kinesis::video;
 STATUS
 KvsSinkStreamCallbackProvider::streamConnectionStaleHandler(UINT64 custom_data,
                                                             STREAM_HANDLE stream_handle,
-                                                            UINT64 time_since_last_buffering_ack) {
+                                                            UINT64 time_since_last_buffering_ack)
+{
     UNUSED_PARAM(custom_data);
-    LOG_DEBUG("Reported streamConnectionStale callback for stream handle " << stream_handle << ". Time since last buffering ack in 100ns: " << time_since_last_buffering_ack);
+    LOG_WARN("Reported streamConnectionStale callback for stream handle " << stream_handle << ". Time since last buffering ack in 100ns: " << time_since_last_buffering_ack);
     return STATUS_SUCCESS;
 }
 
@@ -18,12 +19,14 @@ KvsSinkStreamCallbackProvider::streamErrorReportHandler(UINT64 custom_data,
                                                         STREAM_HANDLE stream_handle,
                                                         UPLOAD_HANDLE upload_handle,
                                                         UINT64 errored_timecode,
-                                                        STATUS status_code) {
+                                                        STATUS status_code)
+{
     LOG_ERROR("Reported stream error. Errored timecode: " << errored_timecode << " Status: 0x" << std::hex << status_code);
-    auto customDataObj = reinterpret_cast<KvsSinkCustomData*>(custom_data);
+    auto customDataObj = reinterpret_cast<KvsSinkCustomData *>(custom_data);
 
     // ignore if the sdk can recover from the error
-    if (!IS_RECOVERABLE_ERROR(status_code)) {
+    if (!IS_RECOVERABLE_ERROR(status_code))
+    {
         customDataObj->stream_status = status_code;
     }
 
@@ -33,7 +36,8 @@ KvsSinkStreamCallbackProvider::streamErrorReportHandler(UINT64 custom_data,
 STATUS
 KvsSinkStreamCallbackProvider::droppedFrameReportHandler(UINT64 custom_data,
                                                          STREAM_HANDLE stream_handle,
-                                                         UINT64 dropped_frame_timecode) {
+                                                         UINT64 dropped_frame_timecode)
+{
     UNUSED_PARAM(custom_data);
     LOG_WARN("Reported droppedFrame callback for stream handle " << stream_handle << ". Dropped frame timecode in 100ns: " << dropped_frame_timecode);
     return STATUS_SUCCESS; // continue streaming
@@ -42,19 +46,19 @@ KvsSinkStreamCallbackProvider::droppedFrameReportHandler(UINT64 custom_data,
 STATUS
 KvsSinkStreamCallbackProvider::streamLatencyPressureHandler(UINT64 custom_data,
                                                             STREAM_HANDLE stream_handle,
-                                                            UINT64 current_buffer_duration) {
+                                                            UINT64 current_buffer_duration)
+{
     UNUSED_PARAM(custom_data);
-    LOG_DEBUG("Reported streamLatencyPressure callback for stream handle " << stream_handle << ". Current buffer duration in 100ns: " << current_buffer_duration);
+    LOG_WARN("Reported streamLatencyPressure callback for stream handle " << stream_handle << ". Current buffer duration in 100ns: " << current_buffer_duration);
     return STATUS_SUCCESS;
 }
 
 STATUS
 KvsSinkStreamCallbackProvider::streamClosedHandler(UINT64 custom_data,
                                                    STREAM_HANDLE stream_handle,
-                                                   UPLOAD_HANDLE upload_handle) {
+                                                   UPLOAD_HANDLE upload_handle)
+{
     UNUSED_PARAM(custom_data);
     LOG_DEBUG("Reported streamClosed callback for stream handle " << stream_handle << ". Upload handle " << upload_handle);
     return STATUS_SUCCESS;
 }
-
-
